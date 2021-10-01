deeaa7a73b30828f3536ef3c00a2ab0f61f7bb51
Zucchinetti Hervé (48186043) <herve.zucchinetti@bobst.com>
2021-10-01 08:44:21 +0200

    Reworking examples

--- examples/usecase_1.py
+++ examples/usecase_1.py
@@ -9,8 +9,12 @@ from warnings import warn
 # imports needed classes
 from asyevent import Event, EventManager, EventWrapperMixin, Callback, IParsable
 
+
 # creates an event manager
 manager = EventManager()
+
+# creates a global event
+start_event = manager.create_event('starting')
 
 
 class Data(IParsable):
@@ -34,24 +38,27 @@ class Data(IParsable):
         return cls(value)
 
 
-class Server:
+class Server(EventWrapperMixin):
     """
-    An example server class which is listening for data
+    An example server class which is listening for data.
+    It inherits from `EventWrapperMixin` in order to use `self` param.
     """
-    def __init__(self):
-        # creates an event for data received
-        self.data_received = manager.create_event('data_received')
-        # creates an event for data lost
-        self.data_lost = manager.create_event('data_lost')
-        # creates an event for data ok
-        self.data_ok = manager.create_event('data_ok')
-        # creates an event for server shut down, handle_errors set to false avoid exceptions to be catch and pass
-        # into the manager error handler
-        self.shutdown = manager.create_event('shut_down', handle_errors=False)
+    def __init__(self, m: EventManager):
+        super().__init__()
 
+        # creates events
+        self.data_received = m.create_event('data_received')
+        self.data_lost = m.create_event('data_lost')
+        self.data_ok = m.create_event('data_ok')
+
+        # handle_errors set to false avoid exceptions to be catch and pass
+        self.shutdown = m.create_event('shut_down', handle_errors=False)
+
+    # `start_delay` is the time took the the callback to start perform
+    @start_event.as_class_callback(start_delay=1)
     async def process_data(self, data: str):
         """
-        An example method which process data
+        An example method which processes data
         """
 
         print('\n---- starting process ----\n')
@@ -64,17 +71,17 @@ class Server:
         await self.shutdown.raise_event()
 
 
-server = Server()
-
-
-class DataProcess(EventWrapperMixin):
+class EventsWrapper(EventWrapperMixin):
     """
     A class that wrap event/command callbacks.
-    It inherits from `EventWrapperMixin` to be able to use `self` param.
+    It inherits from `EventWrapperMixin` in order to use `self` param.
     """
 
-    # add `on_data_received` coroutine as a callback for the event `data_received`.
-    # type hinting data with `Data` force the parsing.
+    server = Server(manager)
+
+    # add `on_data_received` coroutine as a callback for the event `on_data_received`.
+    # type hinting data with `Data` forces the parsing from the passed parameter to a `Data` type.
+    # `Event.before` is an event which is raised before its parent event.
     @server.data_received.before.as_class_callback()
     async def on_data_received(self, data: Data):
         print(f'Data received : {data!r}.')
@@ -84,13 +91,14 @@ class DataProcess(EventWrapperMixin):
         # simulate processing time
         await asyncio.sleep(1)
 
+        # random choice between lose or not data
         if len(data.value) + random.randint(0, 10) % 2 == 0:
             # raises the data lost event if the data are lost
-            await server.data_lost.raise_event()
+            await self.server.data_lost.raise_event()
 
         else:
             # raises the data lost event if the data are OK
-            await server.data_ok.raise_event()
+            await self.server.data_ok.raise_event()
 
     @server.data_lost.as_class_callback()
     async def on_data_lose(self):
@@ -100,48 +108,54 @@ class DataProcess(EventWrapperMixin):
     async def on_data_ok(self):
         print('Data OK !')
 
-    # uses `.after` event which refers to an event that is raised
-    # when the parent event's (data_lost) callbacks are ended
-    # start delay is the delay before the callback
+    # uses `.after` event which refers to an event that is raised,
+    # the first argument is the time took by the previous event's callbacks
+    # when all if the parent event's callbacks finish
     @server.data_received.after.as_class_callback()
-    async def after_data_received(self, time_took: float, data: Data):
+    async def after_data_received(self, time_took: int, data: Data):
         print(f'Processing {data} took {time_took} seconds')
 
+    # before any parent's event (shutdown) is invoked
+    @server.shutdown.before.as_class_callback()
+    async def shutting_down(self):
+        print('Shutting down...')
 
-# uses `.before` event which refers to an event that is raised
-# before any parent's event (shutdown) is invoked
-@server.shutdown.before.as_callback()
-async def shutting_down():
-    print('Shutting down...')
+        # simulate shutting down processing time
+        await asyncio.sleep(3)
 
-    # simulate shutting down processing time
-    await asyncio.sleep(3)
-
-
-@server.shutdown.as_callback()
-async def ready_to_shutdown():
-    print('Server shut down.')
+    @server.shutdown.as_class_callback()
+    async def ready_to_shutdown(self):
+        print('Server shut down.')
 
 
-# the manager error handler event. It is raised when a error occur in a manager's event
-# only if the param `handle_errors` is set to true (default).
+# the manager error handler event.
+# It is raised when a error occur in a manager's event only if the param `handle_errors` is set to true (default).
 # this event pass in the parameters the error, the event and the callback where the error happened.
 # args and kwargs are the arguments/keyword arguments passed when the event has be raised.
 @manager.error_handler.as_callback()
 async def on_error(error: Exception, event: Event, callback: Callback, *args, **kwargs):
     warn(Warning(
-        f'An {type(error)} error occurred while raising {event.event_name!r} event -> Callback : {callback.__name__!r}. '
-        f'Passed args : {args, kwargs}.'
+        f'An {type(error)} error occurred while raising {event.event_name!r} event -> Callback : {callback.__name__!r}.'
+        f'\nPassed args : {args, kwargs}.'
+        f'\n {error}'
     ))
 
 
+async def main():
+    # raises the start event
+    await start_event.raise_event('A cool data')
+
+
 if __name__ == '__main__':
-    # initializes wrapped callbacks, this is a absolute mandatory step in order to use classmethod callbacks
-    DataProcess().init_callbacks()
+    events = EventsWrapper()
+
+    # initializes wrapped callbacks, this is a mandatory step in order to use classmethod callbacks
+    events.init_callbacks()
+    events.server.init_callbacks()
 
     # displays the registered events and commands
     print(f'Registered events : {[e.event_name for e in manager.events]}')
     print(f'Registered commands : {[e.command_name for e in manager.commands]}.')
 
-    # sends data to the server
-    manager.loop.run_until_complete(server.process_data('A cool data'))
+    # main
+    manager.loop.run_until_complete(main())
