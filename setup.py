75cde55eba5f7732ee5315790518bb0976ea191c
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2021-11-22 19:42:42 +0100

    versioning to 0.2.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="asyevent",
-    version="0.2.8",
+    version="0.2.9",
     description="An implementation of events and asynchronous callbacks using decorators.",
     long_description=README,
     long_description_content_type="text/markdown",
